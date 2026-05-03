8f1a1703083e6fe8ed6c4e58da84f0e131b959e5
Sourabh Singh <sourabhsingh@gmail.com>
2026-05-03 12:52:44 +0530

    registerdemo  added commit

--- app.js
+++ app.js
@@ -30,16 +30,16 @@ app.get("/", (req, res) => {
     });
 });
 
-//securty
-app.set('trust proxy', 1);
-app.use(rateLimiter({
-    windowMs:15*60*100, //15 minute
-    max:100, //limit each ip to 100 request
-}))
-app.use(cors());
-app.use(xssclean());
-app.use(helmet());
-//
+// //securty
+// app.set('trust proxy', 1);
+// app.use(rateLimiter({
+//     windowMs:15*60*100, //15 minute
+//     max:100, //limit each ip to 100 request
+// }))
+// app.use(cors());
+// app.use(xssclean());
+// app.use(helmet());
+// //
 
 
 
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const {register, login, registerdemo} = require('../controllers/authController');
 
 router.post("/register", register);
-router.get("/registerdemo", registerdemo);
+//router.get("/registerdemo", registerdemo);
 
 router.post("/login", login);
+
+
+router.get("/registerdemo", (req, res) => {
+    console.log("HIT REGISTER DEMO");
+    res.json({ ok: true });
+});
 
 
 module.exports = router;
